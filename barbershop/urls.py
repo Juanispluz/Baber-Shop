7dd950a33cc61d62bf3cd8afcb0c7872ba4426fb
Miguel Ángel <miguelhito54@gmail.com>
2025-05-19 23:12:39 -0500

    Servicios
    Crear Servicios con estructura de cards o listas, realizar el diseño de la pagina de servicios

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     # Inicio
-    path("", views.index, name="index")
-
+    path("", views.index, name="index"),
+    path('services/', views.index, name='services'),
+    path('service-list/', views.service_list, name='service_list'),
+    path('service/<int:service_id>/', views.service_detail, name='service_detail'),
 ]
