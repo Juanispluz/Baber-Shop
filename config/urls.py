5d6de38e3465a299761b10ec3f923c553085df1f
kerlyd <141505064+kerlyd@users.noreply.github.com>
2025-05-23 08:36:51 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('barbershop.urls'))
+    path("", include('barbershop.urls')
+    path('home/', views.home, name='home'),
 ]
